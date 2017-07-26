b0d77848f993e2a64a26ab401c89b693c0d14008
Tudor Tihan <tudortihan@gmail.com>
2017-07-27 02:46:35 +0300

    Added Server URL per product flavor.

---
+++ app/src/local/java/com/goalsmaster/goalsmaster/utils/ServerSettings.java
@@ -0,0 +1,9 @@
+package com.goalsmaster.goalsmaster.utils;
+
+/**
+ * Created by tudor on 5/9/2017.
+ */
+
+public class ServerSettings {
+    public static final String BASE_PATH = "http://127.0.0.1/goalsmaster/api/1.1.0/";
+}
